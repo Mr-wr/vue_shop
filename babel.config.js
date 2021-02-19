625cd49e77a7c338e32a28192564a05ba8d055ef
武人 <49179402+Mr-wr@users.noreply.github.com>
2021-02-19 17:39:25 +0800

    添加transform-remove-console插件去除打印代码

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,13 @@
-module.exports = {
-  presets: [
-    '@vue/cli-plugin-babel/preset'
-  ]
+const proPlugins = [];
+// 发布阶段需要的插件
+if (process.env.NODE_ENV === "production") {
+  proPlugins.push("transform-remove-console");
 }
+
+module.exports = {
+  presets: ["@vue/cli-plugin-babel/preset"],
+  plugins: [
+    // 发布产品时用的插件
+    ...proPlugins,
+  ],
+};
